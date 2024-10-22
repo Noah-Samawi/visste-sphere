169cf1ebac6fa89b3f674a58fe8ca924aab5190a
Noah Samawi <noah.alsamawi@gmail.com>
2024-10-22 14:05:50 +0200

    Add Google site verification file and update views and urls

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('privacy/', views.privacy, name='privacy'),
+    path('google621ef7f3bda07ae5.html', views.google_verification, name='google_verification'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,23 +1,30 @@
 """Home views"""
 from django.shortcuts import render
 from django.core.exceptions import PermissionDenied
+from django.http import HttpResponse
+import os
 
 
 def home(request):
     """ A view to return the home page """
-
     return render(request, 'home/home.html')
 
 
 def privacy(request):
-    """ A view to return the home page """
-
+    """ A view to return the privacy page """
     return render(request, 'home/privacy.html')
 
 
+# Google verification view
+def google_verification(request):
+    """ A view to return the Google verification file """
+    file_path = os.path.join('static', 'google621ef7f3bda07ae5.html')
+    with open(file_path, 'r') as file:
+        content = file.read()
+    return HttpResponse(content, content_type="text/html")
+
+
 # Error handling views
-
-
 def handler404(request):
     '''
     Render 404 page
@@ -38,5 +45,4 @@ def handler403(request, exception):
     '''
     if isinstance(exception, PermissionDenied):
         return render(request, '403.html', status=403)
-
     return render(request, '500.html', status=500)
